c079c1d636cf87d8039566b7599d38a40988bac8
Raghav Misra <raghav.m2014@gmail.com>
2021-06-28 19:26:37 -0700

    Write reset css

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+    <div id="root">
+        <Nuxt />
+    </div>
+</template>
+
+<style scoped>
+#root {
+    width: 100%;
+    height: 100vh;
+    
+    overflow-x: hidden;
+    overflow-y: auto;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="section">
+        <button style="--type: var(--success)">hey there</button>
+    </div>
+</template>
