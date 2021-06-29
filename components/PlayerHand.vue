ff8466bf49de090cd880d0675c65e3164beb6b24
Raghav Misra <raghav.m2014@gmail.com>
2021-06-29 00:07:51 -0700

    Copy in results and fight page

---
+++ components/PlayerHand.vue
@@ -0,0 +1,83 @@
+<template>
+    <footer>
+        <span v-if="!connection.currentPlayer.activeCard" class="helpText">
+            Click on a card from your hand to play it!
+        </span>
+
+        <div class="deck">
+            <Card
+                v-for="(card, i) in connection.currentPlayer.deck"
+                :key="i"
+                :card="card"
+                :size="15"
+                :rotation="getCardRotation(i)"
+                :show-details="true"
+                :custom-style="`z-index: ${i + 100};`"
+                @click="setActive(i)"
+                @mouseenter="playHoverSound"
+            />
+        </div>
+    </footer>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import connection from "~/services/connection";
+import audio from "~/services/audio";
+export default Vue.extend({
+    computed: {
+        connection() {
+            return connection;
+        },
+    },
+    methods: {
+        playHoverSound() {
+            audio.hover.play();
+        },
+        getCardRotation(i: number): number {
+            if (connection.currentPlayer) {
+                const midpoint = (connection.currentPlayer.deck.length - 1) / 2;
+                return 2.5 * (i - midpoint);
+            }
+            return 0;
+        },
+        setActive(index: number) {
+            connection.room?.send("setActive", { index });
+            audio.placeCard.play();
+        },
+    },
+});
+</script>
+
+<style scoped>
+.helpText {
+    color: white;
+}
+footer {
+    justify-self: flex-end;
+    align-self: stretch;
+    position: relative;
+    height: 2.5rem;
+}
+.deck {
+    background: var(--success-dark);
+    position: absolute;
+    left: 0;
+    right: 0;
+    bottom: -15rem;
+    height: 17.5rem;
+    transition: bottom 0.25s ease-in-out;
+    box-shadow: 0 -5px 20px rgba(1, 9, 32, 0.5);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+.card {
+    margin: 0 -2rem;
+    transition: transform 0.25s ease-in-out;
+}
+.card:hover {
+    transform: translateY(-20rem) scale(1.25) !important;
+    z-index: 500 !important;
+}
+</style>
---
+++ pages/_id/results.vue
@@ -0,0 +1,29 @@
+<template>
+    <main>
+        <ol v-if="results">
+            <li v-for="(player, i) in Object.entries(results)" :key="i">
+                <b>{{ player[0] }}</b>
+                : {{ player[1] }} points
+            </li>
+        </ol>
+    </main>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import connection from "~/services/connection";
+
+export default Vue.extend({
+    computed: {
+        results() {
+            return connection.results;
+        },
+    },
+});
+</script>
+
+<style scoped>
+main {
+    padding: 4rem 8rem;
+}
+</style>
